dcae7416bf080d9711d083dbee1705309df9a39a
Papec21 <idunno2137@gmail.com>
2026-03-17 17:59:53 +0100

    Added waiting for spce to see if it works

--- scraper.py
+++ scraper.py
@@ -1,16 +1,20 @@
 from playwright.sync_api import sync_playwright
+import keyboard
 
 with sync_playwright() as p:
     # Open a new page
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(headless=False)
     page = browser.new_page()
 
     # Visit the target page
-    url = "https://www.pracuj.pl/"
+    url = "https://www.pracuj.pl/praca/poznan;wp?rd=0"
     page.goto(url)
 
     # Data extraction logic
     # Data export logic
 
     # Close the browser 
-    browser.close()
+    # browser.close()
+
+    # Mockup wait to see if it works
+    keyboard.wait('space')
