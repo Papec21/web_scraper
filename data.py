2747b27b1dabf7130b5ac45f97f109a4e05166e6
Papec21 <idunno2137@gmail.com>
2026-03-27 19:50:26 +0100

    Added connection function and env file

--- data.py
+++ data.py
@@ -1,3 +1,22 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+
+def database_connection():
+    try:
+        load_dotenv()
+        connection = psycopg2.connect(
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            host = os.getenv("DB_HOST"),
+            port = os.getenv("DB_PORT"),
+            database = os.getenv("DB_DATABASE")
+        )
+
+        return connection
+    
+    except Exception as error:
+        print(f'Connection error: {error}')
+
+if __name__ == '__main__':
+    database_connection()
